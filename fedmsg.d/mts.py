d52e9d675c62e38f9307e64c3353b446f8a258f9
Chenxiong Qi <cqi@redhat.com>
2019-01-23 13:32:19 +0800

    Refactor fedmsg.d config
    Signed-off-by: Chenxiong Qi <cqi@redhat.com>

--- fedmsg.d/mts.py
+++ fedmsg.d/mts.py
@@ -1,46 +1,32 @@
 # -*- coding: utf-8 -*-
 
 import os
-import warnings
 
 config = {
+    "validate_signatures": False,
+    "active": True,
+    "environment": "prod",
+    "endpoints": {
+        "fedora-infrastructure": [
+            # Just listen to staging for now, not to production (spam!)
+            # "tcp://hub.fedoraproject.org:9940",
+            "tcp://stg.fedoraproject.org:9940",
+        ],
+        'relay_outbound': ["tcp://127.0.0.1:4001"],
+    },
+    'relay_inbound': ["tcp://127.0.0.1:2003"],
+
     'mts-consumer': True,
 }
 
-
-if 'MTS_RH' in os.environ:
-    stomp_uri = os.environ.get('MTS_STOMP_URI')
-    if not stomp_uri:
-        warnings.warn(
-            'Missing environment variable MTS_STOMP_URI for UMB brokers.',
-            RuntimeWarning)
-
-    stomp_ssl_crt = os.environ.get('MTS_STOMP_SSL_CRT')
-    if not stomp_ssl_crt:
-        warnings.warn(
-            'Missing environment variable MTS_STOMP_SSL_CRT for certificate file.',
-            RuntimeWarning)
-    if not os.path.exists(stomp_ssl_crt):
-        raise ValueError(f'Certificate file {stomp_ssl_crt} does not exist.')
-
-    stomp_ssl_key = os.environ.get('MTS_STOMP_SSL_KEY')
-    if not stomp_ssl_key:
-        warnings.warn(
-            'Missing environment variable MTS_STOMP_SSL_KEY for private key file.',
-            RuntimeWarning)
-    if not os.path.exists(stomp_ssl_key):
-        raise ValueError(f'Private key file {stomp_ssl_key} does not exist.')
-
+if 'MTS_USE_STOMP' in os.environ:
     config.update({
-        'validate_signatures': False,
         'zmq_enabled': False,
-
         'stomp_heartbeat': 1000,
-
         # UMB brokers. Could be a comma-separated string of broker URIs.
-        'stomp_uri': stomp_uri,
+        'stomp_uri': os.environ['MTS_STOMP_URI'],
         # File name with absolute path to certificate file.
-        'stomp_ssl_crt': stomp_ssl_crt,
+        'stomp_ssl_crt': os.environ['MTS_STOMP_SSL_CRT'],
         # File name with absolute path to private key file.
-        'stomp_ssl_key': stomp_ssl_key,
+        'stomp_ssl_key': os.environ['MTS_STOMP_SSL_KEY'],
     })
