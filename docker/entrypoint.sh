9554911c5a5c7d33a911c202548236a8dd3e83e1
Chenxiong Qi <cqi@redhat.com>
2019-03-29 11:54:36 +0800

    Adjust gunicorn command line options
    * Restore to use just 1 worker as MTS web endpoint is simple enough to response
      GET request and return data from memory.
    * Make log level configurable by environment variable.
    
    Signed-off-by: Chenxiong Qi <cqi@redhat.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,4 +3,4 @@
 script_dir=$(dirname $(realpath $0))
 
 $script_dir/install-ca.sh && exec fedmsg-hub-3 &
-gunicorn-3 --log-level debug -b 0.0.0.0:8080 --workers 4 message_tagging_service.web:app
+gunicorn-3 --log-level ${GUNICORN_LOG_LEVEL:-info} -b 0.0.0.0:8080 message_tagging_service.web:app
